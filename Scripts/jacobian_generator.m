2570ff1ec2aff11d4da19648353730a54d78ca6b
TonyLuna0905 <anluna1998@gmail.com>
2021-09-29 21:37:40 -0700

    Actualización función de Jacobiano
    Actualización que elimina la necesidad de un parámetro de entrada extra (matriz de transformación de 0 a 6), y la lista de matrices necesaria es de las transofrmaciones de cada referencial con respecto al referencial inercial.

--- Scripts/jacobian_generator.m
+++ Scripts/jacobian_generator.m
@@ -1,15 +1,13 @@
 %--Cálculo de Jacobiano Geométrico--
 %   Necesario:
-%       -Array de matrices de transformación en la forma MTH = {[mth0_1] [mth1_2]...
-%       -Matriz de transformación homogenea de 0 a 6 mth0_6 = mth0_1*mth1_2*mth2_3*mth3_4*mth4_5*mth5_6
+%       -Array de matrices de transformación en la forma MTH = {[mth0_1] [mth0_2]...
 %       -Matriz de vectores directores LAMBDA_R = [lambda_r1 lambda_r2 lambda_r3 lambda_r4 lambda_r5 lambda_r6]
-function J = jacobian_generator(MTH, mth0_6, LAMBDA_R)
+function J = jacobian_generator(MTH, LAMBDA_R)
     J = zeros(6, length(LAMBDA_R));
-    mth0_i = eye(4);
-    d_p = mth0_6(1:3,4);
+    d_p = MTH{end}(1:3,4);
 
     for i = 1:length(LAMBDA_R)
-        mth0_i = mth0_i * MTH{i};
+        mth0_i = MTH{i};
         mth_r_i = mth0_i(1:3,1:3);
         lambda_r_i = LAMBDA_R(:,i);
         d_i = mth0_i(1:3,4);
