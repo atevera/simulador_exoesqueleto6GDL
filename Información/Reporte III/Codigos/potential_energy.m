e09be4f96e852941182dcd81e7fa6c4c793abb83
ATevera <atevera.rz@gmail.com>
2021-10-23 17:55:39 -0500

    Medio Término

--- Información/Reporte III/Codigos/potential_energy.m
+++
@@ -1,9 +0,0 @@
-function U = potential_energy(m, fkcm, g0)
-    n = length(m);
-    U = 0;
-    for i = 1:n
-        mth_i = reshape(fkcm(i,:), 4, 4);
-        d_i = mth_i(1:3,4)';
-        U = U - m(i)*d_i*g0;
-    end
-end
